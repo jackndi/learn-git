4dd81edcc75e6cffe8fc73d307da8183ef4755a3
Jackson Kabuku <ndirangu240@gmail.com>
2020-10-03 14:35:21 +0200

    added even function

--- index.py
+++ index.py
@@ -3,5 +3,14 @@ def main():
     print("Main")
 
 
+def is_even(number):
+    """ check is the number is even"""
+    return True if number % 2 == 0 else False
+
+
 if __name__ == "__main__":
     main()
+
+    # test the is_even
+    print("1 ", is_even(1))
+    print("4 ", is_even(4))
