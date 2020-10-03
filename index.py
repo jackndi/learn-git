bff62c9e349286f913ce7cc3ed95f9f2528551a6
Jackson Kabuku <ndirangu240@gmail.com>
2020-10-03 14:43:55 +0200

    added a odd function

--- index.py
+++ index.py
@@ -8,9 +8,18 @@ def is_even(number):
     return True if number % 2 == 0 else False
 
 
+def is_odd(number):
+    """ check is the number is odd"""
+    return False if number % 2 == 0 else True
+
+
 if __name__ == "__main__":
     main()
 
     # test the is_even
     print("1 ", is_even(1))
     print("4 ", is_even(4))
+
+    # test the is_odd
+    print("1 ", is_odd(1))
+    print("4 ", is_odd(4))
